f89bf677369928ba21b8a65374193dd32cec05c1
Someshfengde <someshfengde@gmail.com>
2023-08-19 14:03:00 +0530

    pushing the API

--- agent_generation.py
+++ agent_generation.py
@@ -1,35 +1,60 @@
-from langchain.embeddings import HuggingFaceEmbeddings, SentenceTransformerEmbeddings
+#%%
+from flask import Flask, request, jsonify
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.llms import OpenAI
 from langchain.chains import ConversationalRetrievalChain
-from langchain.document_loaders import TextLoader
 from glob import glob
+from langchain.memory import ConversationBufferMemory
+from langchain import OpenAI
 from langchain.vectorstores import Chroma
 
 from dotenv import load_dotenv
 load_dotenv('.env')
-loaders = [TextLoader(x) for x in glob("converted_texts/*.txt")]
 
-docs = []
-for loader in loaders:
-    docs.extend(loader.load())
+from langchain.document_loaders import DirectoryLoader
+from langchain.chains import RetrievalQAWithSourcesChain
+#%%
 
-
-text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+loader = DirectoryLoader("./converted_texts",glob = "*.txt")
+docs = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 documents = text_splitter.split_documents(docs)
+db = Chroma.from_documents(documents, OpenAIEmbeddings())
+llm = OpenAI(temperature=0)
 
-embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-vectorstore = Chroma.from_documents(documents, embeddings)
-from langchain.memory import ConversationBufferMemory
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
-qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0), vectorstore.as_retriever(), memory=memory)
-chat_history = []
+# chain = ConversationalRetrievalChain.from_llm(llm, db.as_retriever(search_type="mmr"), memory=ConversationBufferMemory(memory_key="chat_history", return_messages=True))
 
-while True:
-    query = input("You: ")
-    if query == "exit":
-        break
-    chat_history.append(query)
-    result = qa({"question": query, "chat_history": chat_history})
-    print("Bot: ", result["answer"])
+#%%
+
+app = Flask(__name__)
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.json
+    prompt = data['prompt']
+    qa_chain = RetrievalQAWithSourcesChain.from_chain_type(llm, retriever=db.as_retriever())#, memory=ConversationBufferMemory(memory_key="chat_history", return_messages=True))
+    response = qa_chain({"question": prompt})
+    # Merge 'answer' and 'sources' keys into a single dictionary
+    memory_output = {'response': response['answer'], 'sources': response['sources']}
+    
+    # Save the context with the merged dictionary as output
+    # qa_chain.memory.save_context({"question": prompt}, memory_output)
+    
+    return jsonify(memory_output)
+
+
+
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify({"hello": "world"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+# %%
+
+
